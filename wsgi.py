f8a76bbb47bf4033bbf020f671e784c0631a8830
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2020-06-13 15:25:17 -0400

    hope

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,18 @@
 from flask import Flask, request, render_template
 import requests
+from app.main import get_signal
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+@app.route('/', methods=['POST'])
 def func():
-    return render_template('app/template/home.html')
+     ticker = request.form['ticker']
+     interval = request.form['interval']
+     return get_signal(ticker, interval)
 
 if __name__ == "__main__":
     app.run(debug=True)
