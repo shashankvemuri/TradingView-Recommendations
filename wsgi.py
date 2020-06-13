7d5062883471a975ad66013bacd56b3dff5ad1e7
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2020-06-13 16:24:52 -0400

    fifth

--- wsgi.py
+++ wsgi.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/recommendation', methods=['POST'])
+@app.route('/', methods=['POST'])
 def func():
      ticker = request.form['ticker']
      interval = request.form['interval']
